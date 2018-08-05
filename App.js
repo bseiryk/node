24d174021806a8ce642bd12bcf45f5b8036a9d6d
unknown <pheerbah@gmail.com>
2018-08-05 19:21:59 +0300

    added mLab database

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const config = require("config")
-const app = express();
+const mongoose = require('mongoose')
 const todos = require('./routes/todos')
 const mainDebuger = require('debug')('main')
+
+
+mongoose.connect('mongodb://nevis1:nevis789456123@ds020228.mlab.com:20228/node_learn', {
+    useNewUrlParser: true
+});
+mongoose.connection.once('open', () => console.log('connected to db'))
+
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
